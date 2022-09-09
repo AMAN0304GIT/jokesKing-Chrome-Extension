2c07eaf211c25c75a10ea1d9d7401724fee87b31
AMAN0304GIT <65027853+AMAN0304GIT@users.noreply.github.com>
2022-09-09 16:33:52 +0530

    Delete JokesKingExtension directory

--- JokesKingExtension/script.js
+++
@@ -1,9 +0,0 @@
-fetch('https://icanhazdadjoke.com/slack')
-    .then(data => data.json())
-    .then(jokeData => {
-        const jokeText = jokeData.attachments[0].text;
-        const JokeElement = document.getElementById('jokeElement');
-
-        JokeElement.innerHTML = jokeText;
-        
-    })
